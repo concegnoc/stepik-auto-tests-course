a3e4fdeb773227c4f6cafc6559574f6ff9861218
concegnoc <ppk-00@mail.ru>
2020-10-25 16:05:23 +0300

    add last task in module 3.6

--- test_items.py
+++ test_items.py
@@ -3,4 +3,5 @@ import time
 def test_should_check_basket_of_product_with_certain_language(browser):
     browser.find_element_by_css_selector("[class='btn btn-lg btn-primary btn-add-to-basket']")
     assert True
+
     time.sleep(30)
