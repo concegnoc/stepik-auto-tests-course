7041f0e84f5005ffbf204df7969bee5f79d18bfc
concegnoc <72815955+concegnoc@users.noreply.github.com>
2020-10-26 11:33:21 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 import time
 
 def test_should_check_bottom_of_adding_of_product_with_certain_language(browser):
-    button = browser.find_element_by_css_selector("[class='btn btn-lg btn-primary btn-add-to-basket']")
-    assert button
+    browser.find_element_by_css_selector("[class='btn btn-lg btn-primary btn-add-to-basket']")
+    assert True
 
     time.sleep(30)
