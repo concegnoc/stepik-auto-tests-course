5d6deda5448010c7064cf3ea8db7a7aafa4086e6
concegnoc <72815955+concegnoc@users.noreply.github.com>
2020-10-26 11:41:45 +0300

    Update test_items.py
    change name of function

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 import time
 
-def test_should_check_bottom_of_adding_of_product_with_certain_language(browser):
+def test_should_check_bottom_of_adding_product_with_certain_language(browser):
     button = browser.find_element_by_css_selector("[class='btn btn-lg btn-primary btn-add-to-basket']")
     assert button
 
